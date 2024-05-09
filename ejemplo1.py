168e17f9ca1c0ce2e02658869d60534da2e7e66e
Tomas Gonzalez Alarado <csctomasgonzalez@gmail.com>
2024-05-09 05:26:26 -0600

    cambios en Ejemplo1.py

--- ejemplo1.py
+++ ejemplo1.py
@@ -9,8 +9,12 @@ class Auto:
     def metodo_publico(selft):
         print("Este es un método público");
 
+    def autoavanza(selft):
+            print("El auto está avanzando")
+
 #se instancía un objeto
 taxi = Auto()
 
 print("Taxi 456 modelo {} con placa {}".format(taxi.modelo, taxi.placa))
 taxi.metodo_publico()
+taxi.autoavanza()
